7731fd611a8df6ea2ce54b960f57788bf37437f4
clementbiteau <clement.biteau.mars@gmail.com>
2025-04-10 16:59:55 +0200

    committing to better pull

--- srcs/algo.py
+++ srcs/algo.py
@@ -1,153 +1,140 @@
-from copy import deepcopy
-
-# Deep copy game
-
-
-def makeMove(self, x, y, player):
-    """Record move from player"""
-    self.board[x][y] = player
+# def makeMove(self, x, y, player):
+#     """Record move from player"""
+#     self.board[x][y] = player
 
 
-def undoMove(self, x, y):
-    """Undo the move from AI"""
-    self.board[x][y] = "."
+# def undoMove(self, x, y):
+#     """Undo the move from AI"""
+#     self.board[x][y] = "."
 
 
-def isDone(self):
-    """Collect info if game is done or still running"""
-    if self.ingame is False:
-        return False
+# def isDone(self):
+#     """Collect info if game is done or still running"""
+#     if self.ingame is False:
+#         return False
 
 
-def getPossibleMoves(self):
-    """Check the empty spots on the board to signal them as possible moves for the AI"""
-    return [
-        (x, y)
-        for y in range(len(self.board))
-        for x in range(len(self.board[0]))
-        if self.board[x][y] == "."
-    ]
+# def getPossibleMoves(self):
+#     """Check the empty spots on the board to signal them as possible moves for the AI"""
+#     return [
+#         (x, y)
+#         for y in range(len(self.board))
+#         for x in range(len(self.board[0]))
+#         if self.board[x][y] == "."
+#     ]
 
 
-def checkBoard(self, player):
-    """Simple heuristic checker for our board to see which placement could be more beneficial"""
-    opponent = "p1" if player == "p2" else "p2"
-    symbol = self.getSymbolFromPlayer(player)
-    opp_symbol = self.getSymbolFromPlayer(opponent)
+# def checkBoard(self, player):
+#     """Simple heuristic checker for our board to see which placement could be more beneficial"""
+#     opponent = "p1" if player == "p2" else "p2"
+#     symbol = self.getSymbolFromPlayer(player)
+#     opp_symbol = self.getSymbolFromPlayer(opponent)
 
-    score = 0
+#     score = 0
 
-    score += self.p1_piece * 50 if player == "p1" else self.p2_piece * 50
-    score -= self.p2_piece * 50 if player == "p1" else self.p1_piece * 50
+#     score += self.p1_piece * 50 if player == "p1" else self.p2_piece * 50
+#     score -= self.p2_piece * 50 if player == "p1" else self.p1_piece * 50
 
-    for y in range(len(self.board)):
-        for x in range(len(self.board[0])):
-            if self.board[y][x] == symbol:
-                align_len = self.checkAlignments(symbol, (y, x))
-                if align_len:
-                    score += align_len * 100  # Congrats that's a good move :)
-            elif self.board[y][x] == opp_symbol:
-                align_len = self.checkAlignments(opp_symbol, (y, x))
-                if align_len:
-                    score -= (
-                        align_len * 80
-                    )  # This is not a good move so we deduct score from the play
+#     for y in range(len(self.board)):
+#         for x in range(len(self.board[0])):
+#             if self.board[y][x] == symbol:
+#                 align_len = self.checkAlignments(symbol, (y, x))
+#                 if align_len:
+#                     score += align_len * 100  # Congrats that's a good move :)
+#             elif self.board[y][x] == opp_symbol:
+#                 align_len = self.checkAlignments(opp_symbol, (y, x))
+#                 if align_len:
+#                     score -= (
+#                         align_len * 80
+#                     )  # This is not a good move so we deduct score from the play
 
-    if self.pending_win and self.pending_win["player"] == player:
-        score += 5000
-    elif self.pending_win and self.pending_win["player"] == opponent:
-        score -= 5000
+#     if self.pending_win and self.pending_win["player"] == player:
+#         score += 5000
+#     elif self.pending_win and self.pending_win["player"] == opponent:
+#         score -= 5000
 
-    return score
+#     return score
 
 
-def minimax(game, depth, alpha, beta, maxim):
-    """The minimax algorithm works as follows: A Game Tree
-    On one side, we have the root situation (the current game state we are in);
-    Then, the study of the next moves that could be seen as nodes from that tree.
-    At each depth, we iterate bewteen maximizing (maxim) and minimizing player.
+# def minimax(game, depth, alpha, beta, maxim):
+#     """The minimax algorithm works as follows: A Game Tree
+#     On one side, we have the root situation (the current game state we are in);
+#     Then, the study of the next moves that could be seen as nodes from that tree.
+#     At each depth, we iterate bewteen maximizing (maxim) and minimizing player.
 
-    The 2 players are against each other:
-    Maximizing Player => Aims to maximize their advantage;
-    Minimizing Player => Aims to minimize the maximizing player's advantage
+#     The 2 players are against each other:
+#     Maximizing Player => Aims to maximize their advantage;
+#     Minimizing Player => Aims to minimize the maximizing player's advantage
 
-    We store the evident advantage in a score variable.
+#     We store the evident advantage in a score variable.
 
-    We have 2 states : game is finished vs game is not finished.
-    In the first case, the game was either won, tied or lost;
-    In the second case, the game is ongoing and the algo will return the score afer evaluating the state of the game.
+#     We have 2 states : game is finished vs game is not finished.
+#     In the first case, the game was either won, tied or lost;
+#     In the second case, the game is ongoing and the algo will return the score afer evaluating the state of the game.
 
-    Maximizing Player's move : the algo looks for the moves that would increase
-    the maximizing player's chances of winning.
-    Minimizing Player's move : the algo looks for the moves that would decrease
-    those chances by returning the lowest score.
+#     Maximizing Player's move : the algo looks for the moves that would increase
+#     the maximizing player's chances of winning.
+#     Minimizing Player's move : the algo looks for the moves that would decrease
+#     those chances by returning the lowest score.
 
-    This is done in a recursive fashion => when depth == 0 => evaluation is over and game state has been evaluated.
+#     This is done in a recursive fashion => when depth == 0 => evaluation is over and game state has been evaluated.
 
-    Depth == 0
-    When Depth reaches zero, the evaluation is done. The game state is ready. The scores have been given.
-    A positive score will be given for the Maximizing,
-    A negative score will be given for the Minimizing,
-    A zero score if it is a tie.
+#     Depth == 0
+#     When Depth reaches zero, the evaluation is done. The game state is ready. The scores have been given.
+#     A positive score will be given for the Maximizing,
+#     A negative score will be given for the Minimizing,
+#     A zero score if it is a tie.
 
-    The algo returns the top_move variable that has been found in the tree.
+#     The algo returns the top_move variable that has been found in the tree.
 
-    We use the Alpha-Beta pruning method.
-    Alpha-Beta => ignoring the branches that do not give an interesting score whatsoever.
-    Alpha = best value for Maximizing Player;
-    Beta = best value for Minimizing Player;
+#     We use the Alpha-Beta pruning method.
+#     Alpha-Beta => ignoring the branches that do not give an interesting score whatsoever.
+#     Alpha = best value for Maximizing Player;
+#     Beta = best value for Minimizing Player;
 
-    In our case, if Beta <= Alpha,
-    We break and stop the evaluation on this branch because we will not find anything better.
-    """
-    # If given depth is Zero we return the current game state and None as nothing will be evaluated
-    if depth == 0 or game.isDone():
-        return game.checkBoard(game.whoPlay), None
-    # @top_move is declared to store the highest score guarantee
-    top_move = None
-    # entering the Maximizing player's turn
-    if maxim:
-        max_check = float(
-            "-inf"
-        )  # setting the max_check score to a minimum for optimal use
-        for move in game.getPossibleMoves():
-            x, y = move
-            game.makeMove(x, y, game.whoPlay)
-            eval, _ = minimax(game, depth - 1, alpha, beta, False)
-            game.undoMove(x, y)
+#     In our case, if Beta <= Alpha,
+#     We break and stop the evaluation on this branch because we will not find anything better.
+#     """
+#     # If given depth is Zero we return the current game state and None as nothing will be evaluated
+#     if depth == 0 or game.isDone():
+#         return game.checkBoard(game.whoPlay), None
+#     # @top_move is declared to store the highest score guarantee
+#     top_move = None
+#     # entering the Maximizing player's turn
+#     if maxim:
+#         max_check = float(
+#             "-inf"
+#         )  # setting the max_check score to a minimum for optimal use
+#         for move in game.getPossibleMoves():
+#             x, y = move
+#             game.makeMove(x, y, game.whoPlay)
+#             eval, _ = minimax(game, depth - 1, alpha, beta, False)
+#             game.undoMove(x, y)
 
-            if eval > max_check:
-                max_check = eval
-                top_move = move
-            alpha = max(alpha, eval)
+#             if eval > max_check:
+#                 max_check = eval
+#                 top_move = move
+#             alpha = max(alpha, eval)
 
-            if beta <= alpha:
-                break
+#             if beta <= alpha:
+#                 break
 
-        return max_check, top_move
+#         return max_check, top_move
 
-    else:  # opposite of maxim player except when we cut to the chase by using alpha beta pruning
-        min_check = float("inf")
-        for move in game.getPossibleMoves():
-            x, y = move
-            game.makeMove(x, y, game.whoPlay)
-            eval, _ = minimax(game, depth - 1, alpha, beta, True)
-            game.undoMove(x, y)
+#     else:  # opposite of maxim player except when we cut to the chase by using alpha beta pruning
+#         min_check = float("inf")
+#         for move in game.getPossibleMoves():
+#             x, y = move
+#             game.makeMove(x, y, game.whoPlay)
+#             eval, _ = minimax(game, depth - 1, alpha, beta, True)
+#             game.undoMove(x, y)
 
-            if eval < min_check:
-                min_check = eval
-                top_move = move
-            beta = min(beta, eval)
+#             if eval < min_check:
+#                 min_check = eval
+#                 top_move = move
+#             beta = min(beta, eval)
 
-            if beta <= alpha:
-                break
+#             if beta <= alpha:
+#                 break
 
-        return min_check, top_move
-
-def getAImove(self, game, depth):
-    depth = game.getDifficulty(depth)
-    game_copy = deepcopy(game)
-    _, move = minimax(
-        game_copy, depth, alpha=float("-inf"), beta=float("inf"), maxim=True
-    )  # deepcopy only once good for opti Time Complexity
-    return move
+#         return min_check, top_move
--- srcs/game.py
+++ srcs/game.py
@@ -1,8 +1,8 @@
 import random
 import pygame
 import config
+from copy import deepcopy
 from utile import placeButtonAtPercent, draw_text_in_rect, show_notification
-from algo import getAImove
 
 class Game:
     def __init__(self):
@@ -78,9 +78,16 @@ class Game:
         self.inGame = True
         self.time.start()
         
+        if self.startPlayer():
+            self.whoStart = "p1"
+            self.whoPlay = "p1"
+        else:
+            self.whoStart = "p2"
+            self.whoPlay = "ai"
+        self.isAIgame = True        
+        
     def setAIdifficulty(self, difficulty):
         self.AIdifficulty = difficulty
-        self.isAIgame = True
         
     def getDifficulty(self):
         return {
@@ -88,6 +95,155 @@ class Game:
         "MOYEN": 3,
         "IMPOSSIBLE": 5,
     }.get(self.AIdifficulty, 1)
+
+    def makeMove(self, x, y, player):
+        """Record move from player"""
+        self.board[x][y] = player
+
+
+    def undoMove(self, x, y):
+        """Undo the move from AI"""
+        self.board[x][y] = "."
+
+
+    def isDone(self):
+        """Collect info if game is done or still running"""
+        if self.inGame is False:
+            return False
+
+
+    def getPossibleMoves(self):
+        """Check the empty spots on the board to signal them as possible moves for the AI"""
+        return [
+            (x, y)
+            for y in range(len(self.board))
+            for x in range(len(self.board[0]))
+            if self.board[x][y] == "."
+        ]
+
+
+    def checkBoard(self, player):
+        """Simple heuristic checker for our board to see which placement could be more beneficial"""
+        opponent = "p1" if player == "p2" else "p2"
+        symbol = self.getSymbolFromPlayer(player)
+        opp_symbol = self.getSymbolFromPlayer(opponent)
+
+        score = 0
+
+        score += self.p1_piece * 50 if player == "p1" else self.p2_piece * 50
+        score -= self.p2_piece * 50 if player == "p1" else self.p1_piece * 50
+
+        for y in range(len(self.board)):
+            for x in range(len(self.board[0])):
+                if self.board[y][x] == symbol:
+                    align_len = self.checkAlignments(symbol, (y, x))
+                    if align_len:
+                        score += align_len * 100  # Congrats that's a good move :)
+                elif self.board[y][x] == opp_symbol:
+                    align_len = self.checkAlignments(opp_symbol, (y, x))
+                    if align_len:
+                        score -= (
+                            align_len * 80
+                        )  # This is not a good move so we deduct score from the play
+
+        if self.pending_win and self.pending_win["player"] == player:
+            score += 5000
+        elif self.pending_win and self.pending_win["player"] == opponent:
+            score -= 5000
+
+        return score
+
+
+    def minimax(self, game, depth, alpha, beta, maxim):
+        """The minimax algorithm works as follows: A Game Tree
+        On one side, we have the root situation (the current game state we are in);
+        Then, the study of the next moves that could be seen as nodes from that tree.
+        At each depth, we iterate bewteen maximizing (maxim) and minimizing player.
+
+        The 2 players are against each other:
+        Maximizing Player => Aims to maximize their advantage;
+        Minimizing Player => Aims to minimize the maximizing player's advantage
+
+        We store the evident advantage in a score variable.
+
+        We have 2 states : game is finished vs game is not finished.
+        In the first case, the game was either won, tied or lost;
+        In the second case, the game is ongoing and the algo will return the score afer evaluating the state of the game.
+
+        Maximizing Player's move : the algo looks for the moves that would increase
+        the maximizing player's chances of winning.
+        Minimizing Player's move : the algo looks for the moves that would decrease
+        those chances by returning the lowest score.
+
+        This is done in a recursive fashion => when depth == 0 => evaluation is over and game state has been evaluated.
+
+        Depth == 0
+        When Depth reaches zero, the evaluation is done. The game state is ready. The scores have been given.
+        A positive score will be given for the Maximizing,
+        A negative score will be given for the Minimizing,
+        A zero score if it is a tie.
+
+        The algo returns the top_move variable that has been found in the tree.
+
+        We use the Alpha-Beta pruning method.
+        Alpha-Beta => ignoring the branches that do not give an interesting score whatsoever.
+        Alpha = best value for Maximizing Player;
+        Beta = best value for Minimizing Player;
+
+        In our case, if Beta <= Alpha,
+        We break and stop the evaluation on this branch because we will not find anything better.
+        """
+        # If given depth is Zero we return the current game state and None as nothing will be evaluated
+        if depth == 0 or game.isDone():
+            return game.checkBoard(game.whoPlay), None
+        # @top_move is declared to store the highest score guarantee
+        top_move = None
+        # entering the Maximizing player's turn
+        if maxim:
+            max_check = float(
+                "-inf"
+            )  # setting the max_check score to a minimum for optimal use
+            for move in game.getPossibleMoves():
+                x, y = move
+                game.makeMove(x, y, game.whoPlay)
+                eval, _ = self.minimax(game, depth - 1, alpha, beta, False)
+                game.undoMove(x, y)
+
+                if eval > max_check:
+                    max_check = eval
+                    top_move = move
+                alpha = max(alpha, eval)
+
+                if beta <= alpha:
+                    break
+
+            return max_check, top_move
+
+        else:  # opposite of maxim player except when we cut to the chase by using alpha beta pruning
+            min_check = float("inf")
+            for move in game.getPossibleMoves():
+                x, y = move
+                game.makeMove(x, y, game.whoPlay)
+                eval, _ = self.minimax(game, depth - 1, alpha, beta, True)
+                game.undoMove(x, y)
+
+                if eval < min_check:
+                    min_check = eval
+                    top_move = move
+                beta = min(beta, eval)
+
+                if beta <= alpha:
+                    break
+
+            return min_check, top_move
+        
+    def getAImove(self, game, depth):
+        depth = game.getDifficulty()
+        game_copy = deepcopy(game)
+        _, move = self.minimax(
+            game_copy, depth, alpha=float("-inf"), beta=float("inf"), maxim=True
+        )  # deepcopy only once good for opti Time Complexity
+        return move
 
     def startPlayer(self):
         """Détermine aléatoirement qui commence."""
@@ -140,13 +296,20 @@ class Game:
 
     def nextTurn(self):
         self.turn += 1
-        self.whoPlay = (
-            self.whoStart
-            if (self.turn % 2)
-            else "p1"
-            if (self.whoStart == "p2")
-            else "p2"
-        )
+        if self.isAIgame:
+            if self.whoPlay == "ai":
+                self.whoPlay = "p1" if self.whoStart == "p2" else "p2"
+            else:
+                self.whoPlay = "ai"
+                
+        else:
+            self.whoPlay = (
+                self.whoStart
+                if (self.turn % 2)
+                else "p1"
+                if (self.whoStart == "p2")
+                else "p2"
+            )
 
     def playAt(self, coords):
         symbol = "1" if self.whoPlay == "p1" else "2"
@@ -200,9 +363,12 @@ class Game:
                 self.nextTurn()
                 
             if self.whoPlay == "ai" and self.inGame:
-                ai_move = getAImove(self.board)
+                depth = self.getDifficulty()
+                ai_move = self.getAImove(self, depth)
                 if ai_move:
                     self.playAt(ai_move)
+                else:
+                    self.nextTurn()
         else:
             show_notification("Double free three interdit !")
 
